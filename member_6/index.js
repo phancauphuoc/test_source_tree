ca09b08d6e29beb649a08317d71af458b817f191
PhanPhuoc <ldcc.phuocphan@lotte.vn>
2024-05-06 21:41:25 +0700

    reset

---
+++ member_6/index.js
@@ -0,0 +1,8 @@
+var linkElement = document.getElementById('myLink');
+
+// Thêm thuộc tính href
+linkElement.setAttribute('href', 'https://example.co');
+
+// Thêm thuộc tính target
+linkElement.setAttribute('target', '_blank');
+linkElement.style.color = 'red';
